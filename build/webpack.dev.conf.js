5174711de112184b14854ccc3466b7b4dce30b2f
mangoyiy <mangoyiy@163.com>
2018-02-05 23:27:49 +0800

    vue-lazyload showFilterPop

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -10,6 +10,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
+const express = require('express')
+const app = express()
+var appData = require('./../mock/goods.json')
+var goods = appData.goods
+var apiRoutes = express.Router()
+app.use('/api', apiRoutes)
+
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
 
@@ -42,6 +49,14 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
+    },
+    before: function (app) {
+      app.get('/api/goods', (req, res) => {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
     }
   },
   plugins: [
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns:v-lazy="http://www.w3.org/1999/xhtml">
   <div>
     <nav-header></nav-header>
     <nav-bread>
@@ -10,25 +10,16 @@
           <span class="sortby">Sort by:</span>
           <a href="javascript:void(0)" class="default cur">Default</a>
           <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-          <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+          <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
         </div>
         <div class="accessory-result">
           <!-- filter -->
-          <div class="filter stopPop" id="filter">
+          <div class="filter stopPop" id="filter" v-bind:class="{'filterby-show': filterBy}">
             <dl class="filter-price">
               <dt>Price:</dt>
-              <dd><a href="javascript:void(0)">All</a></dd>
-              <dd>
-                <a href="javascript:void(0)">0 - 100</a>
-              </dd>
-              <dd>
-                <a href="javascript:void(0)">100 - 500</a>
-              </dd>
-              <dd>
-                <a href="javascript:void(0)">500 - 1000</a>
-              </dd>
-              <dd>
-                <a href="javascript:void(0)">1000 - 2000</a>
+              <dd><a href="javascript:void(0)" v-bind:class="{'cur': priceChecked == 'all'}" @click="priceChecked = 'all' ">All</a></dd>
+              <dd v-for="(price, index) in priceFilter">
+                <a href="javascript:void(0)" v-bind:class="{'cur': priceChecked==index}"  @click="setPriceFilter(index)">{{price.startPrice}} - {{price.endPrice}}</a>
               </dd>
             </dl>
           </div>
@@ -37,60 +28,27 @@
           <div class="accessory-list-wrap">
             <div class="accessory-list col-4">
               <ul>
-                <li>
+
+                <li v-for="(item,index) in goodsList">
                   <div class="pic">
-                    <a href="#"><img src="static/1.jpg" alt=""></a>
+                    <a href="#"><img v-lazy="'/static/'+item.productImg" alt=""></a>
                   </div>
                   <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">999</div>
+                    <div class="name">{{item.productName}}</div>
+                    <div class="price">{{item.productPrice}}</div>
                     <div class="btn-area">
                       <a href="javascript:;" class="btn btn--m">加入购物车</a>
                     </div>
                   </div>
                 </li>
-                <li>
-                  <div class="pic">
-                    <a href="#"><img src="static/2.jpg" alt=""></a>
-                  </div>
-                  <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">1000</div>
-                    <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="pic">
-                    <a href="#"><img src="static/3.jpg" alt=""></a>
-                  </div>
-                  <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">500</div>
-                    <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="pic">
-                    <a href="#"><img src="static/4.jpg" alt=""></a>
-                  </div>
-                  <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">2499</div>
-                    <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                    </div>
-                  </div>
-                </li>
+
               </ul>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
     <nav-footer></nav-footer>
   </div>
 </template>
@@ -103,16 +61,59 @@
     import NavFooter from './../components/NavFooter'
     import NavBread  from './../components/NavBread'
 
+    import axios from 'axios'
+
     export default {
         data() {
           return {
-            msg: 'goods list'
+            goodsList: [],
+            priceFilter:[
+              {
+                startPrice: '0.00',
+                endPrice: '500.00'
+              },
+              {
+                startPrice: '500.00',
+                endPrice: '1000.00'
+              },
+              {
+                startPrice: '1000.00',
+                endPrice: '2000.00'
+              }
+            ],
+            priceChecked: 'all',
+            filterBy: false,
+            overLayFlag: false
           }
         },
         components: {
           NavHeader:NavHeader,
           NavFooter:NavFooter,
           NavBread:NavBread
+        },
+        mounted: function() {
+          this.getGoodsList()
+        },
+        methods: {
+          getGoodsList() {
+            axios.get("/api/goods").then((result) => {
+              var res = result.data.data;
+              console.log(res)
+              this.goodsList = res.result;
+            })
+          },
+          showFilterPop() {
+            this.filterBy = true;
+            this.overLayFlag = true;
+          },
+          setPriceFilter(index) {
+            this.priceChecked = index
+            this.closePop();
+          },
+          closePop() {
+            this.filterBy = false;
+            this.overLayFlag = false;
+          }
         }
     }
 </script>
