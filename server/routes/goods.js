e02b8c60f7d8ae3f9cb6680beaff8c072330f736
mangoyiy <mangoyiy@163.com>
2018-02-12 23:43:11 +0800

    Pagination and sort back-end interface

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -25,10 +25,25 @@ mongoose.connection.on("disconnected", function () {
 // 实现路由
 router.get("/",function (req, res, next) {     // 当访问 "/" 的时候就默认访问到goods信息
   // res.send("hello good list......");
+  console.log(req);
+
+  let page = parseInt(req.param("page"));       // 分页
+  let pageSize = parseInt(req.param("pageSize"));
+
+  let skip = (page - 1)*pageSize;
+
+  let sort = req.param("sort");  // 接收前端传过来的字段
+
+  let params = {};
+  let goodsModel = Goods.find(params).skip(skip).limit(pageSize);   // 括号内的skip表示默认跳过skip条数据. limit表示一页pageSize条数据
+                                                                    // 所以skip()和limit()之间的配合，skip=1表明是第一页，那么其实一条数据都不跳过
+                                                                    // 此时pageSize的条数为固定的，比如最多是8条。
+
+  goodsModel.sort({"salePrice": sort})  // 括号里面的sort是前端传过来的，1表示升序，-1表示降序。在nodejs中传递的数据都是以对象的形式
 
   // 模型的find api去查找.   在mongo中返回的doc,称之为文档
-  Goods.find({}, function (err, doc) {
-
+  // Goods.find({}, function (err, doc) {
+  goodsModel.exec(function (err, doc) {
     if(err) {
       res.json({        // res.json 是可以直接输出json文件
         status: '1',
